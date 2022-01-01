9bc2ced13a9f3b921d189fd6d0b1e39065b3dfbc
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-01-01 23:50:45 +0000

    Add tests for the copy function

--- tests/registry.cpp
+++ tests/registry.cpp
@@ -1,9 +1,7 @@
 #include <apecs/apecs.hpp>
 #include <gtest/gtest.h>
 
-struct foo {
-    int value = 0;
-};
+struct foo { int value = 0; };
 struct bar {};
 
 TEST(registry, entity_invalid_after_destroying)
@@ -62,64 +60,6 @@ TEST(registry, test_noexcept_get)
 
     bar* bar_get = reg.get_if<bar>(e);
     ASSERT_EQ(bar_get, nullptr);
-}
-
-TEST(registry_view, view_for_loop)
-{
-    apx::registry<foo, bar> reg;
-
-    auto e1 = reg.create();
-    reg.emplace<foo>(e1);
-    reg.emplace<bar>(e1);
-
-    auto e2 = reg.create();
-    reg.emplace<bar>(e2);
-
-    std::size_t count = 0;
-    for (auto entity : reg.view<foo>()) {
-        ++count;
-    }
-    ASSERT_EQ(count, 1);
-}
-
-TEST(registry_view, view_for_loop_multi)
-{
-    apx::registry<foo, bar> reg;
-
-    auto e1 = reg.create();
-    reg.emplace<foo>(e1);
-    reg.emplace<bar>(e1);
-
-    auto e2 = reg.create();
-    reg.emplace<bar>(e2);
-
-    auto e3 = reg.create();
-    reg.emplace<foo>(e3);
-    reg.emplace<bar>(e3);
-
-    std::size_t count = 0;
-    for (auto entity : reg.view<foo, bar>()) {
-        ++count;
-    }
-    ASSERT_EQ(count, 2);
-}
-
-TEST(registry_all, all_for_loop)
-{
-    apx::registry<foo, bar> reg;
-
-    auto e1 = reg.create();
-    reg.emplace<foo>(e1);
-    reg.emplace<bar>(e1);
-
-    auto e2 = reg.create();
-    reg.emplace<bar>(e2);
-
-    std::size_t count = 0;
-    for (auto entity : reg.all()) {
-        ++count;
-    }
-    ASSERT_EQ(count, 2);
 }
 
 TEST(registry, test_add)
@@ -181,4 +121,86 @@ TEST(registry, multi_destroy_initializer_list)
     ASSERT_EQ(reg.size(), 3);
 
     reg.destroy({e1, e2, e3});
+}
+
+TEST(registry_iteration, view_for_loop)
+{
+    apx::registry<foo, bar> reg;
+
+    auto e1 = reg.create();
+    reg.emplace<foo>(e1);
+    reg.emplace<bar>(e1);
+
+    auto e2 = reg.create();
+    reg.emplace<bar>(e2);
+
+    std::size_t count = 0;
+    for (auto entity : reg.view<foo>()) {
+        ++count;
+    }
+    ASSERT_EQ(count, 1);
+}
+
+TEST(registry_iteration, view_for_loop_multi)
+{
+    apx::registry<foo, bar> reg;
+
+    auto e1 = reg.create();
+    reg.emplace<foo>(e1);
+    reg.emplace<bar>(e1);
+
+    auto e2 = reg.create();
+    reg.emplace<bar>(e2);
+
+    auto e3 = reg.create();
+    reg.emplace<foo>(e3);
+    reg.emplace<bar>(e3);
+
+    std::size_t count = 0;
+    for (auto entity : reg.view<foo, bar>()) {
+        ++count;
+    }
+    ASSERT_EQ(count, 2);
+}
+
+TEST(registry_iteration, all_for_loop)
+{
+    apx::registry<foo, bar> reg;
+
+    auto e1 = reg.create();
+    reg.emplace<foo>(e1);
+    reg.emplace<bar>(e1);
+
+    auto e2 = reg.create();
+    reg.emplace<bar>(e2);
+
+    std::size_t count = 0;
+    for (auto entity : reg.all()) {
+        ++count;
+    }
+    ASSERT_EQ(count, 2);
+}
+
+TEST(registry_copying, copying_entities_within_reg)
+{
+    apx::registry<foo> reg;
+
+    auto e1 = reg.create();
+    reg.add<foo>(e1, {});
+
+    auto e2 = apx::copy(e1, reg, reg);
+    ASSERT_TRUE(reg.valid(e2));
+    ASSERT_TRUE(reg.has<foo>(e2));
+}
+
+TEST(registry_copying, copying_entities_different_reg)
+{
+    apx::registry<foo> reg1, reg2;
+
+    auto e1 = reg1.create();
+    reg1.add<foo>(e1, {});
+
+    auto e2 = apx::copy(e1, reg1, reg2);
+    ASSERT_TRUE(reg2.valid(e2));
+    ASSERT_TRUE(reg2.has<foo>(e2));
 }
